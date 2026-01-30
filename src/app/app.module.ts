34124622c853059162e6672db7d12e83aab4ea4b
DungDev <hoangdinhdung0205@gmail.com>
2026-01-30 15:55:25 +0700

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authenticate/login/login.component';
+import { ServiceComponent } from './service/service.component';
+import { RegisterComponent } from './authenticate/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ServiceComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
